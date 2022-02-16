1ccde7403750ff0de00d57d34773b136f5eb0108
oleg.knyazev <oleg.knyazev@myoffice.team>
2022-02-16 23:08:30 +0300

    GJG-31337 | refactor: refactor code, add logs, use zerolog as logger, add verbose flag

---
+++ internal/repos_search/repos_search.go
@@ -0,0 +1,57 @@
+package repos_search
+
+import (
+	"fmt"
+	"io/fs"
+	"os"
+	"path/filepath"
+)
+
+func CrawlPath(srcPath string) (map[string][]string, error) {
+	pathsMap := make(map[string][]string)
+
+	hosts, err := os.ReadDir(srcPath)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, v := range hosts {
+		err := detectGitRepos(filepath.Join(srcPath, v.Name()), pathsMap)
+		if err != nil {
+			return nil, err
+		}
+	}
+
+	return pathsMap, nil
+}
+
+func detectGitRepos(path string, pathsMap map[string][]string) error {
+	return filepath.WalkDir(path, func(path string, d fs.DirEntry, err error) error {
+		var dif []os.DirEntry
+
+		stat, err := os.Stat(path)
+		if err != nil {
+			fmt.Printf("failed to get fsstat of path: %s, %s", path, err.Error())
+			return nil
+		}
+
+		if stat.IsDir() {
+			if dif, err = os.ReadDir(path); err != nil {
+				fmt.Printf("failed to read dir %s\n", path)
+			}
+			for _, v := range dif {
+				if v.Name() == ".git" {
+					fp := filepath.Base(path)
+
+					if _, ok := pathsMap[fp]; !ok {
+						pathsMap[fp] = []string{path}
+					} else {
+						pathsMap[fp] = append(pathsMap[v.Name()], path)
+					}
+					break
+				}
+			}
+		}
+		return nil
+	})
+}
